92c616abde559f2cd434ba0ab3ecabf9f0d84fc4
Mddct <hamddct@gmail.com>
2025-06-12 21:19:15 +0800

    addd simple conformer dit

---
+++ exps/conformer_ae/dataset.py
@@ -0,0 +1,154 @@
+import json
+from functools import partial
+
+import torch
+import torchaudio
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import DataLoader
+from wenet.dataset.datapipes import WenetRawDatasetSource
+
+
+def decode_wav(sample):
+    obj = json.loads(sample['line'])
+    filepath = obj['wav']
+    audio, sample_rate = torchaudio.load(filepath)
+    return {
+        'wav': audio,
+        "sample_rate": sample_rate,
+    }
+
+
+def resample(sample, resample_rate=16000):
+    """ Resample sample.
+        Inplace operation.
+
+        Args:
+            sample: {key, wav, label, sample_rate}
+            resample_rate: target resample rate
+
+        Returns:
+            {key, wav, label, sample_rate}
+    """
+    assert 'sample_rate' in sample
+    assert 'wav' in sample
+    sample_rate = sample['sample_rate']
+    waveform = sample['wav']
+    if sample_rate != resample_rate:
+        sample['sample_rate'] = resample_rate
+        sample['wav'] = torchaudio.transforms.Resample(
+            orig_freq=sample_rate, new_freq=resample_rate)(waveform)
+    return sample
+
+
+def filter_by_length(sample, max_seconds=30, min_seconds=0.5):
+    wav = sample['wav']
+    sr = sample['sample_rate']
+    duration = wav.shape[1] / sr
+    if duration <= max_seconds and duration >= min_seconds:
+        return True
+    return False
+
+
+def sort_by_feats(sample):
+    assert 'wav' in sample
+    assert isinstance(sample['wav'], torch.Tensor)
+    return sample['wav'].size(1)
+
+
+def pad_waveform_to_divisor(waveform: torch.Tensor,
+                            divisor: int) -> torch.Tensor:
+    original_len = waveform.shape[-1]
+    if original_len % divisor == 0:
+        return waveform
+    remainder = original_len % divisor
+    pad_len = divisor - remainder
+    padded_waveform = torch.nn.functional.pad(waveform, (0, pad_len),
+                                              mode='constant',
+                                              value=0)
+    return padded_waveform
+
+
+def compute_raw(sample, dims):
+    sample['wav'] = pad_waveform_to_divisor(sample['wav'],
+                                            dims).reshape(1, -1, dims)
+    return sample
+
+
+def padding(data, pad_value=0.0):
+
+    wavs_lst = [sample['wav'][0] for sample in data]
+    wavs_lens = [sample['wav'].shape[1] for sample in data]
+
+    feats = pad_sequence(wavs_lst, batch_first=True, padding_value=pad_value)
+    feats_lens = torch.tensor(wavs_lens, dtype=torch.int64)
+    return {
+        'audio': feats,
+        'audio_lens': feats_lens,
+    }
+
+
+class DynamicBatchWindow:
+
+    def __init__(self, max_frames_in_batch=12000):
+        self.longest_frames = 0
+        self.max_frames_in_batch = max_frames_in_batch
+
+    def __call__(self, sample, buffer_size):
+        assert isinstance(sample, dict)
+        assert 'wav' in sample
+        assert isinstance(sample['wav'], torch.Tensor)
+        new_sample_frames = sample['wav'].size(1)
+        self.longest_frames = max(self.longest_frames, new_sample_frames)
+        frames_after_padding = self.longest_frames * (buffer_size + 1)
+        if frames_after_padding > self.max_frames_in_batch:
+            self.longest_frames = new_sample_frames
+            return True
+        return False
+
+
+def init_dataset_and_dataloader(files,
+                                batch_size,
+                                num_workers,
+                                prefetch,
+                                shuffle,
+                                steps,
+                                wav_reshape_dims=160,
+                                drop_last=False,
+                                sample_rate=24000,
+                                seed=2025,
+                                sort_buffer_size=1024,
+                                batch_type='static',
+                                split='train'):
+
+    dataset = WenetRawDatasetSource(files,
+                                    cycle=steps,
+                                    shuffle=shuffle,
+                                    partition=True)
+    dataset = dataset.map(decode_wav)
+    dataset = dataset.filter(filter_by_length)
+    if split == 'train':
+        dataset = dataset.sort(buffer_size=sort_buffer_size,
+                               key_func=sort_by_feats)
+    dataset = dataset.map(partial(resample, resample_rate=sample_rate))
+    dataset = dataset.map(partial(compute_raw, dims=wav_reshape_dims))
+    assert batch_type in ['static', 'dynamic']
+    if batch_type == 'static':
+        dataset = dataset.batch(batch_size,
+                                wrapper_class=partial(padding, pad_value=0.0),
+                                drop_last=drop_last)
+    else:
+        max_frames_in_batch = batch_size
+        dataset = dataset.dynamic_batch(
+            DynamicBatchWindow(max_frames_in_batch),
+            wrapper_class=partial(padding, pad_value=0.0),
+        )
+    generator = torch.Generator()
+    generator.manual_seed(seed)
+
+    dataloader = DataLoader(dataset,
+                            batch_size=None,
+                            num_workers=num_workers,
+                            persistent_workers=True,
+                            prefetch_factor=prefetch,
+                            generator=generator)
+    return dataset, dataloader
---
+++ exps/conformer_ae/train_utils.py
@@ -0,0 +1,221 @@
+import math
+import os
+from typing import Any
+
+import torch
+import torch.distributed as dist
+import torch.optim as optim
+from absl import logging
+from exps.conformer_ae.dataset import init_dataset_and_dataloader
+from exps.conformer_ae.usm_ae_raw import AudioAutoEncoder
+from torch.optim.lr_scheduler import _LRScheduler
+from torch.utils.tensorboard import SummaryWriter
+
+
+class WarmupLR(_LRScheduler):
+    """The WarmupLR scheduler
+
+    This scheduler is almost same as NoamLR Scheduler except for following
+    difference:
+
+    NoamLR:
+        lr = optimizer.lr * model_size ** -0.5
+             * min(step ** -0.5, step * warmup_step ** -1.5)
+    WarmupLR:
+        lr = optimizer.lr * warmup_step ** 0.5
+             * min(step ** -0.5, step * warmup_step ** -1.5)
+
+    Note that the maximum lr equals to optimizer.lr in this scheduler.
+
+    """
+
+    def __init__(
+        self,
+        optimizer: torch.optim.Optimizer,
+        warmup_steps=25000,
+        last_epoch: int = -1,
+    ):
+        self.warmup_steps = warmup_steps
+        # __init__() must be invoked before setting field
+        # because step() is also invoked in __init__()
+        super().__init__(optimizer, last_epoch)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(warmup_steps={self.warmup_steps})"
+
+    def get_lr(self):
+        step_num = self.last_epoch + 1
+        warmup_steps = self.warmup_steps
+        if not isinstance(warmup_steps, List):
+            warmup_steps = [self.warmup_steps] * len(self.base_lrs)
+
+        def initlr_fn(lr):
+            return lr * step_num**-0.5
+
+        def warmuplr_fn(lr, warmup_step):
+            return lr * warmup_step**0.5 * min(step_num**-0.5,
+                                               step_num * warmup_step**-1.5)
+
+        return [
+            initlr_fn(lr) if warmup_steps[i] == 0 else warmuplr_fn(
+                lr, warmup_steps[i]) for (i, lr) in enumerate(self.base_lrs)
+        ]
+
+    def set_step(self, step: int):
+        self.last_epoch = step
+
+
+def init_distributed(configs):
+
+    local_rank = os.environ.get('LOCAL_RANK', 0)
+    world_size = int(os.environ.get('WORLD_SIZE', 1))
+    local_rank = int(os.environ.get('LOCAL_RANK', 0))
+    rank = int(os.environ.get('RANK', 0))
+
+    torch.cuda.set_device(local_rank)
+    dist.init_process_group('nccl')
+    print('training on multiple gpus, this gpu {}'.format(local_rank) +
+          ', rank {}, world_size {}'.format(rank, world_size))
+
+    return world_size, local_rank, rank
+
+
+class TrainState:
+
+    def __init__(
+        self,
+        config: Any,
+    ):
+
+        _, _, self.rank = init_distributed(config)
+        model = AudioAutoEncoder(config)
+        model.cuda()
+        self.config = config
+        # TODO: FSDP or deepspeed for future usm ae or dit (flow) decoder
+        self.model = torch.nn.parallel.DistributedDataParallel(model)
+        self.device = config.device
+
+        self.max_steps = config.max_train_steps
+        _, self.dataloader = init_dataset_and_dataloader(
+            config.train_data,
+            config.per_device_batch_size,
+            config.num_workers,
+            config.prefetch,
+            True,
+            self.max_steps,
+            wav_reshape_dims=config.in_dims,
+            sample_rate=config.sample_rate,
+            seed=config.seed)
+
+        self.step = 0
+        self.timesteps = torch.linspace(
+            1,
+            0,
+            steps=config.flow_infer_steps + 1,
+        ).cuda()
+
+        self.writer = SummaryWriter(config.tensorboard_dir)
+        # Optimizers
+        self.opt = optim.AdamW(
+            self.model.parameters(),
+            lr=config.learning_rate,
+            # betas=(0.8, 0.9),
+        )
+        self.scheduler = WarmupLR(self.opt, config.warmup_steps)
+
+    def train_step(self, batch, device):
+        wav, wav_lens = batch['audio'].to(device), batch['audio_lens'].to(
+            device)
+
+        timesteps = torch.randint(
+            low=0,
+            high=len(self.timesteps) - 1,
+            size=(wav.shape[0], ),
+            device=wav.device,
+        )
+        log_str = f'[RANK {self.rank}] step_{self.step+1}: '
+
+        loss_dict = self.model(wav, wav_lens, self.timesteps[timesteps])
+        loss = loss_dict['loss']
+        loss.backward()
+        grad_norm = torch.nn.utils.clip_grad_norm_(self.model.parameters(),
+                                                   self.config.clip_grad_norm)
+        self.opt.step()
+        self.opt.zero_grad()
+        self.scheduler.step()
+        if self.rank == 0:
+            self.writer.add_scalar("train/loss_flow", loss_dict['loss_flow'],
+                                   self.step)
+            self.writer.add_scalar("train/loss_kl", loss_dict['loss_flow'],
+                                   self.step)
+            self.writer.add_scalar("train/loss", loss_dict['loss'], self.step)
+
+            self.writer.add_scalar("train/grad_norm", grad_norm, self.step)
+
+        log_str += f' loss {loss:>6.3f} loss_flow {loss_dict["loss_flow"]:>6.3f} loss_kl {loss_dict["loss_kl"]:>6.3f}'
+        opt_lrs = [group['lr'] for group in self.opt.param_groups]
+        for i, lr in enumerate(opt_lrs):
+            if self.rank == 0:
+                self.writer.add_scalar('train/lr_{}'.format(i), lr, self.step)
+            log_str += f' lr_{i} {lr:>6.5f}'
+
+        if (self.step + 1) % self.config.log_interval == 0:
+            logging.info(log_str)
+
+    def train(self):
+        if self.config.checkpoint != '':
+            self.resume(self.config.checkpoint)
+        self.model.train()
+        for batch in self.dataloader:
+            dist.barrier()
+            self.train_step(batch, self.config.device)
+            if (self.step + 1) % self.config.checkpoint_every_steps == 0:
+                self.save()
+            self.step += 1
+            if self.step >= self.max_steps:
+                print("Training complete.")
+                return
+
+    def save(self):
+        if self.rank == 0:
+            checkpoint_dir = os.path.join(self.config.model_dir,
+                                          f'step_{self.step}')
+            os.makedirs(checkpoint_dir, exist_ok=True)
+
+            model_state_dict = self.model.module.state_dict()
+            meta = {
+                'model': model_state_dict,
+                'step': self.step,
+            }
+            torch.save(meta, os.path.join(checkpoint_dir, 'model.pt'))
+
+            opt_dict = self.opt.state_dict()
+            torch.save(opt_dict, os.path.join(checkpoint_dir, 'opt.pt'))
+
+            scheduler_state_dict = self.scheduler.state_dict()
+            torch.save(scheduler_state_dict,
+                       os.path.join(checkpoint_dir, 'scheduler.pt'))
+            logging.info(
+                f'[RANK {self.rank}] Checkpoint: save to checkpoint {checkpoint_dir}'
+            )
+
+    def resume(self, checkpoint_dir: str):
+
+        model = self.model.module
+        ckpt = torch.load(os.path.join(checkpoint_dir, 'model.pt'),
+                          map_location='cpu',
+                          mmap=True)
+        model.load_state_dict(ckpt['model'])
+        self.step = ckpt['step'] + 1  # train from new step
+
+        opt = self.opt
+        ckpt = torch.load(os.path.join(checkpoint_dir, 'opt_disc.pt'),
+                          map_location='cpu',
+                          mmap=True)
+        opt.load_state_dict(ckpt)
+
+        logging.info(
+            f'[RANK {self.rank}] Checkpoint: load  checkpoint {checkpoint_dir}'
+        )
+
+        dist.barrier()
